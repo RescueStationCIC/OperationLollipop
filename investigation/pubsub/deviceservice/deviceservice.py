92fca9f43fb8bf7853f9c9dac435a1a8309d2d90
coopsatwork <coopsatwork@users.noreply.github.com>
2023-05-02 11:02:45 +0100

    refactored for reusable subscriber

--- investigation/pubsub/deviceservice/deviceservice.py
+++ investigation/pubsub/deviceservice/deviceservice.py
@@ -6,43 +6,28 @@ from trio import run
 from pynng import Sub0, Timeout
 from common.definitions import Definitions
 from common.messagehandler import MessageHandler
+
+class ConfigHandler(MessageHandler):
+    
+    def on_new_data(self, object):
+        self.on_new_config(object)
+    
+    def __init__(self, topic_name, on_new_config):
+        MessageHandler.__init__(self, topic_name)
+        self.on_new_config = on_new_config
+            
+        
                 
 # uncomment to behave as daemon
 # with daemon.DaemonContext():
 # comment to behave as daemon
 
 
-# def on_new_config(config):
-#     print (config)
+def on_new_config(config):
+    print ('new config, sir!')
 
-# async def config_handler(subscriber, on_new_config, topic_def, encoding):
-#     msg = await subscriber.arecv_msg()
-#     raw = msg.bytes
-#     data = raw.decode(encoding).partition(topic_def)[2]
-
-#     object = json.loads(data)
-#     on_new_config(object)
-    
-# async def begin_config_handler():
-#         # create config publisher
-#     encoding = Definitions.instance().definition('TRANSFER_ENCODING')
-#     topic_def:str = Definitions.instance().definition('TOPIC_CONFIG') + ": "
-#     topic_def_bytes:bytes = topic_def.encode(encoding)
-
-#     config_subscriber = Sub0(
-#         dial=Definitions.instance().definition('PUBSUB_ADDRESS'), 
-#         recv_timeout=Definitions.instance().definition('RECEIVER_TIMEOUT_MS'))
-#     config_subscriber.subscribe(topic_def_bytes)
-    
-#     while (1):
-#         await config_handler(config_subscriber, on_new_config, topic_def, encoding)
-
-
-# def start():
-#     run (begin_config_handler)
-    
 def start():
-    config_handler = MessageHandler(Definitions.instance().definition('TOPIC_CONFIG'))
+    config_handler = ConfigHandler(Definitions.instance().definition('TOPIC_CONFIG'), on_new_config)
     run (config_handler.start)
     
     
